21a4f937fe1cf36f3b1da71f25be144f82ffbbb4
Kevdevui <kevinajcabey@hotmail.co.uk>
2024-08-01 12:49:55 +0100

    Second commit - files have now been added on this upload

---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "This is me, always smilling, gaming and coding for fun ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/kevin_img.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "We can connect from across the world online",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development and life",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "What my brain thinks of code",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "I'm an email away",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Analogue/Digital Clock",
+    des: "A simple clock, displaying the time, date and a toggle for light & dark.",
+    img: "/clock_dark_light.png",
+    iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg"],
+    link: "codepen.io/kevinajcabey/pen/podRoMV",
+  },
+  {
+    id: 2,
+    title: "LifeStyle Photographs",
+    des: "Simplify website I created for a UNI project based on a topic of our choice",
+    img: "/ls_photo.png",
+    iconLists: ["/re.svg", "/html5.svg", "/css3.svg", "/javascript.svg"],
+    link: "lifestylephotographs.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Expanding Cards",
+    des: "A stack of expanding cards, that etend to the left & right on large devices and top to bottom on small devices",
+    img: "/expand_cards.png",
+    iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg"],
+    link: "codepen.io/kevinajcabey/pen/abVpJZg",
+  },
+  {
+    id: 4,
+    title: "Goog Lifestyle Blog",
+    des: "Simple blog site I created to try Javascript page transition and it turned out pretty well",
+    img: "/goodlifestyle.png",
+    iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg"],
+    link: "goodlifestyle.netlify.app",
+  },
+  {
+    id: 5,
+    title: "Todo List",
+    des: "Stylish todo-list that your able to add a task and cross them out once you complete them.",
+    img: "/todo_list.png",
+    iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg"],
+    link: "/codepen.io/kevinajcabey/pen/QWOdwjm",
+  },
+  
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus odio vel pharetra tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam interdum commodo elit, at mollis libero rhoncus vitae.",
+    name: "Michael",
+    title: "Test1",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus odio vel pharetra tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam interdum commodo elit, at mollis libero rhoncus vitae.",
+    name: "Johnson",
+    title: "Test2",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus odio vel pharetra tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam interdum commodo elit, at mollis libero rhoncus vitae.",
+    name: "Anna",
+    title: "Test3",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus odio vel pharetra tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam interdum commodo elit, at mollis libero rhoncus vitae.",
+    name: "Lisa",
+    title: "Test4",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  // {
+  //   id: 5,
+  //   name: "docker.",
+  //   img: "/dock.svg",
+  //   nameImg: "/dockerName.svg",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Alto HTML Template Creator",
+    desc: "Zoopla - Oct 2023 - Present: Create HTML templates in the Alto system according to customer requirements, liaising directly with the customers as required to ensure clear understanding - Ensure templates are populated with the correct information, have the correct styling, and work as expected - To pick up tickets from the support ticketing system that are related to Document or HTML creation for the Alto product - Manage multiple client requests and adhere to committed timescales - Ability to control and manage customers expectations throughout the process - Produce excellent quality documents/templates for the customers - Learn the software products as required to complete the task",
+    className: "md:col-span-2",
+    thumbnail: "/alto.png",
+  },
+  {
+    id: 2,
+    title: "Alto Portal Feed Analyst",
+    desc: "Zoopla - Aug 2022 - Oct 2023: Support clients with the software products - Answer incoming phone calls and respond to support tickets from end users - Log issues, perform basic fault finding and escalate technical problems in sufficient detail where necessary - Identify gaps in software functionality and opportunities to improve the product and service - Assist in issue resolution and the preparation of documentation as directed by the Team Leader - Proactively obtain relevant technical and product knowledge as required for the customer - Carry out any other reasonable duties commensurate with my role",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/zoopla.png",
+  },
+  {
+    id: 3,
+    title: "Alto HTML Template Creator",
+    desc: "Zoopla - Aug 2022 - Oct 2023: Create HTML templates in the Alto system according to customer requirements, liaising directly with the customers as required to ensure clear understanding",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/alto.png",
+  },
+  {
+    id: 4,
+    title: "Senior Stockroom & Delivery",
+    desc: "Urban Outfitters - Oct 2018 - Feb 2022: Organising stock deliveries - Clearing delivery and keeping the workspace tidy - Working as a team to complete daily procedure - Support the team during peak times - Sales assistance",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/urbanoutfitters.png",
+  },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
